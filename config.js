c0b11b990d4f9fa74b187820bac99e2693bf0af0
tvvinkle <arumbit@gmail.com>
2017-05-13 11:54:09 -0400

    initial config

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAANUUw8i3zABAEqprZCaryDxQHXZAOGJPcYcm1xiK1wxtcjxJJ7bSjtmCFjtCZBKvywsdAZCZA4Kxe4QY3JGpyRPXJuXVOV8RRW1dYityLxRlaVARuUiXDmS5W2ZAxnxwuMqVoTleb6941nVK8UMpQKsaZAGyIc4ZCnwlZCuRMz6n3AZDZD',
+    FB_VERIFY_TOKEN: 'hungry_man_need_food_1',
+    API_AI_CLIENT_ACCESS_TOKEN: '2069cb4ee7764343814cb7416babacbd',
+    FB_APP_SECRET: 'b724f2eae132af420e844e784514c170',
+    SERVER_URL: "https://hungryandhungry.herokuapp.com/",
 };
+
+
+    
+
